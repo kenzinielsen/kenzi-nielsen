86ce460c55e0579c53da8ad001175757a7954f0e
Kenzi Nielsen <kenzinielsen@gmail.com>
2021-12-11 14:23:22 -0700

    add components

---
+++ kenzi-nielsen/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <header>
+            <nav>
+                <a>About Me</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
