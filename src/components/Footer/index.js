d39d8347622d2d19c0cde9f3e550dbd8dd8b9ca5
Kenzi Nielsen <kenzinielsen@gmail.com>
2022-01-01 17:58:23 -0700

    color update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4 navbar fixed-bottom">
       <div className="container navbar fixed-bottom" id="footer">&copy;2021 by Kenzi Nielsen
-      <a className="footerlink" href="https://github.com/kenzinielsen">GitHub</a>
+      <a className="footerlinks" href="https://github.com/kenzinielsen">GitHub</a>
       <a className="footerlink" href="https://www.linkedin.com/in/kenzi-nielsen/">LinkedIn</a>
 
       </div>
